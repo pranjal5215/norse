988160bbd3ab94e1bc1eefced9602ea43abcd14c
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2015-07-22 18:32:22 +0530

    redis and general config functionalities

---
+++ backends/redis.go
@@ -0,0 +1,91 @@
+package norse
+
+import (
+	"fmt"
+	"time"
+	"errors"
+
+	"golang.org/x/net/context"
+
+	"github.com/goibibo/hammerpool"
+	"github.com/garyburd/redigo/redis"
+)
+
+
+
+var poolMap map[string]*hammerpool.ResourcePool
+
+type RedisStruct struct {
+	redis.Conn
+}
+
+// Close redis conn
+func (rConn *RedisStruct) Close(){
+	_ = rConn.Conn.Close()
+}
+
+// Specify a factory function to create a connection,
+// context and a timeout for connection to be created
+func init() {
+	// For each type in redis create corresponding pool
+	for key, config := range redisConfigs{
+		factory := func() (hammerpool.Resource, error) {
+			cli, err := redis.Dial("tcp", "127.0.0.1:6379")
+			// select db if specified
+			db, ok := config["db"]
+			if ok{
+				cli.Do("SELECT", config)
+			}else{
+				cli.Do("SELECT", 0)
+			}
+			if err != nil{
+				fmt.Println("Error in Redis Dial")
+			}
+			res := &RedisStruct{cli}
+			return res, nil
+		}
+		t := time.Duration(5000*time.Millisecond)
+		poolMap[key] = hammerpool.NewResourcePool(factory, 10, 100, t)
+	}
+}
+
+// Your instance type for redis
+func GetMyRedis() (*RedisStruct) {
+	return &RedisStruct{}
+}
+
+// Execute, get connection from a pool 
+// fetch and return connection to a pool.
+func (r *RedisStruct) Execute(redisInstance string, cmd string, args ...interface{}) (interface{}, error) {
+	ctx := context.TODO()
+	pool, ok := poolMap[redisInstance]
+	if ok{
+		conn, err := pool.Get(ctx)
+	}else{
+		return nil, errors.New("Redis: instance Not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer pool.Put(conn)
+	return conn.(*RedisStruct).Do(cmd, args...)
+}
+
+// Redis Get,
+func Get(redisInstance string, key string) (string, error){
+	value, err := redis.String(rStruct.Execute("flight", "GET", "a"))
+	if err != nil{
+		return value, nil
+	}else{
+		return nil, err
+	}
+}
+
+// How to use redis,
+func main(){
+	rStruct := GetMyRedis()
+	value, _ := redis.String(rStruct.Execute("flight", "GET", "a"))
+	fmt.Println(value)
+	value, _ = redis.String(rStruct.Execute("flight", "GET", "key"))
+	fmt.Println(value)
+}
