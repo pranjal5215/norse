1d981d8ec232c6eae70384f74714e7add27c113e
Vivek Narain <vivek.narain@goibibo.com>
2015-07-27 12:30:00 +0530

    sql wrapper

---
+++ config/mySqlConf.go
@@ -0,0 +1,31 @@
+package norse
+
+import (
+	"fmt"
+)
+
+var dbname = "mysql"
+
+// Load mysql configs after general config unmarshall
+func LoadSqlConfig() (map[string]map[string]string, error) {
+        // Get configVar from general config
+        configVar, err := loadConfig()
+        if err != nil{
+                return nil, err
+        }
+        mysqlCs := make(map[string]map[string]string)
+        for database, settings := range(configVar[dbname].(map[string]interface{})){
+                settingsI := settings.(map[string]interface{})
+                configMap := make(map[string]string)
+                for databaseConf, configMapI := range(settingsI){
+                        configMap[databaseConf] = configMapI.(string)
+                }
+                mysqlCs[database] = configMap
+        }
+        fmt.Println(mysqlCs["flight"]["port"])
+        return mysqlCs, nil
+}
+/*
+func main(){
+	fmt.Println(LoadSqlConfig())
+}*/
