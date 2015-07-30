766a1cd69b655e83bbc51220488c1929cc3abc45
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2015-07-30 14:44:22 +0530

    add configure

--- config/loadConfig.go
+++ config/loadConfig.go
@@ -2,15 +2,25 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 type jsonConfig map[string]interface{}
 
-var configFilePath = "/home/pranjal/goOld/config.json"
+var configFilePath = ""
+
+func Configure(path string) {
+	configFilePath = path
+}
 
 // Load files from general config
 func loadConfig() (jsonConfig, error) {
+	if configFilePath == "" {
+		fmt.Println("No config specified; pleae call config.Configure(filePath)")
+		os.Exit(1)
+	}
 	file, err := ioutil.ReadFile(configFilePath)
 	temp := make(jsonConfig)
 	if err = json.Unmarshal(file, &temp); err != nil {
