620e0db6124d26be23b3b9a0240c429d00e700e5
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2015-08-10 13:58:36 +0530

    comment

--- config/redisConfig.go
+++ config/redisConfig.go
@@ -4,6 +4,7 @@ package config
 func LoadRedisConfig() (map[string]map[string]string, error) {
 	var dbname = "redis"
 	redisCs := make(map[string]map[string]string)
+	// Convert package conf jsonConfigInstance to individual vertical config.
 	for database, settings := range jsonConfigInstance[dbname].(map[string]interface{}) {
 		settingsI := settings.(map[string]interface{})
 		configMap := make(map[string]string)
