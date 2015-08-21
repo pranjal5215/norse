d1493d4b02be204658c92c84e0f7d5ec633fc47d
Harshad Patil <harshad0402@gmail.com>
2015-08-21 18:43:07 +0530

    redis execute tests

--- backends/redis_test.go
+++ backends/redis_test.go
@@ -1,6 +1,7 @@
 package backends
 
 import (
+	"github.com/garyburd/redigo/redis"
 	"github.com/goibibo/norse/config"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -32,4 +33,23 @@ func Test_GetSet(t *testing.T) {
 	delCount, errd := redisClient.Delete("redisConfig", "key")
 	assert.NoError(t, errd, "redis delete error")
 	assert.Equal(t, 1, delCount, "redis del value")
+}
+
+func Test_Execute(t *testing.T) {
+	redisClient, _ := GetRedisClient(incrFun, decrFun, "redis")
+
+	_, err := redisClient.Execute("redisConfig", "SET", "key", "value")
+	assert.NoError(t, err, "redis set error")
+
+	value, errg := redisClient.Execute("redisConfig", "GET", "key")
+	assert.NoError(t, errg, "redis get error")
+	vals, errs := redis.String(value, nil)
+	assert.NoError(t, errs, "redis string error")
+	assert.Equal(t, "value", vals, value, "redis get value")
+
+	delCount, errd := redisClient.Execute("redisConfig", "DEL", "key")
+	assert.NoError(t, errd, "redis delete error")
+	dcI, erri := redis.Int(delCount, nil)
+	assert.NoError(t, erri, "redis int error")
+	assert.Equal(t, 1, dcI, "redis del value")
 }
