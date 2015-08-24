ed140180ecdefc9e9a6a340ed5bf996b0a1910a7
Harshad Patil <harshad0402@gmail.com>
2015-08-24 14:51:39 +0530

    added tests for redis sets

--- backends/redis_test.go
+++ backends/redis_test.go
@@ -1,10 +1,11 @@
 package backends
 
 import (
+	"testing"
+
 	"github.com/garyburd/redigo/redis"
 	"github.com/goibibo/norse/config"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 var redisCount int
@@ -33,20 +34,24 @@ func Test_GetSet(t *testing.T) {
 	delCount, errd := redisClient.Delete("redisConfig", "key")
 	assert.NoError(t, errd, "redis delete error")
 	assert.Equal(t, 1, delCount, "redis del value")
+
+	redisClient.Execute("redisConfig", "FLUSHDB")
 }
 
-func Test_MGetSet(t *testing.T){
+func Test_MGetSet(t *testing.T) {
 	redisClient, _ := GetRedisClient(incrFun, decrFun, "redis")
 
-  val := map[string]interface{}{ "key1": "value1", "key2": "value2","key3": "value3"}
+	val := map[string]interface{}{"key1": "value1", "key2": "value2", "key3": "value3"}
 	_, err := redisClient.MSet("redisConfig", val)
 	assert.NoError(t, err, "redis MSet error")
 
 	value, errg := redisClient.MGet("redisConfig", "key1", "key2", "key3")
 	assert.NoError(t, errg, "redis get error")
 	assert.Equal(t, "value1", value[0], "redis get value")
-  assert.Equal(t, "value2", value[1], "redis get value")
+	assert.Equal(t, "value2", value[1], "redis get value")
 	assert.Equal(t, "value3", value[2], "redis get value")
+
+	redisClient.Execute("redisConfig", "FLUSHDB")
 }
 
 func Test_Execute(t *testing.T) {
@@ -66,4 +71,66 @@ func Test_Execute(t *testing.T) {
 	dcI, erri := redis.Int(delCount, nil)
 	assert.NoError(t, erri, "redis int error")
 	assert.Equal(t, 1, dcI, "redis del value")
+
+	redisClient.Execute("redisConfig", "FLUSHDB")
+}
+
+func Test_Sets(t *testing.T) {
+	redisClient, _ := GetRedisClient(incrFun, decrFun, "redis")
+
+	saddb, err := redisClient.SAdd("redisConfig", "set", "ele1", "ele2", "ele3", "ele4")
+	assert.Equal(t, true, saddb, "sadd")
+	assert.NoError(t, err, "sadd error")
+
+	set, err1 := redisClient.Sismember("redisConfig", "set", "ele1")
+	assert.Equal(t, true, set, "sismember ")
+	assert.NoError(t, err1, "sadd error")
+
+	set, err1 = redisClient.Sismember("redisConfig", "set", "ele2")
+	assert.Equal(t, true, set, "sismember")
+	assert.NoError(t, err1, "sadd error")
+
+	set, err1 = redisClient.Sismember("redisConfig", "set", "ele3")
+	assert.Equal(t, true, set, "sismember")
+	assert.NoError(t, err1, "sadd error")
+
+	set, err1 = redisClient.Sismember("redisConfig", "set", "ele4")
+	assert.Equal(t, true, set, "sismember")
+	assert.NoError(t, err1, "sadd error")
+
+	sets, err2 := redisClient.Sismembers("redisConfig", "set", []string{"ele1", "ele2", "ele3", "ele4"})
+	assert.Equal(t, []bool{true, true, true, true}, sets, "sismembers")
+	assert.NoError(t, err2, "sadd error")
+
+	redisClient.Execute("redisConfig", "FLUSHDB")
+}
+
+func Test_Sets2(t *testing.T) {
+	redisClient, _ := GetRedisClient(incrFun, decrFun, "redis")
+
+	saddb, err := redisClient.SAdd("redisConfig", "set", "ele1", "ele2", "ele3", "ele4")
+	assert.Equal(t, true, saddb, "sadd")
+	assert.NoError(t, err, "sadd error")
+
+	set, err1 := redisClient.SRem("redisConfig", "set", "ele1")
+	assert.Equal(t, true, set, "srem ")
+	assert.NoError(t, err1, "srem error")
+
+	set, err1 = redisClient.Sismember("redisConfig", "set", "ele1")
+	assert.Equal(t, false, set, "sismember ")
+	assert.NoError(t, err1, "sadd error")
+
+	sets, err2 := redisClient.Sismembers("redisConfig", "set", []string{"ele2", "ele3", "ele4"})
+	assert.Equal(t, []bool{true, true, true}, sets, "sismembers")
+	assert.NoError(t, err2, "sadd error")
+
+	saddb2, err := redisClient.SAdd("redisConfig", "set", "ele1", "ele2", "ele3", "ele4")
+	assert.Equal(t, true, saddb2, "sadd")
+	assert.NoError(t, err, "sadd error")
+
+	set2, err1 := redisClient.Sismember("redisConfig", "set", "ele1")
+	assert.Equal(t, true, set2, "sismember ")
+	assert.NoError(t, err1, "sadd error")
+
+	redisClient.Execute("redisConfig", "FLUSHDB")
 }
